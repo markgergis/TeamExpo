9b9dd963d56e6a505582495120093c6e14dbdb6b
markragaee <markragaee@gmail.com>
2017-10-20 17:31:22 +0200

    1st commit

--- app/src/main/java/com/codecontest/expo/teamexpo/Hexagon.java
+++ app/src/main/java/com/codecontest/expo/teamexpo/Hexagon.java
@@ -1,212 +1,212 @@
-//package com.codecontest.expo.teamexpo;
-//
-//public class Hexagon {
-//
-//	private int id;
-//
-//	private GameCoordinate center = new GameCoordinate(0, 0);
-//
-//	private int guardPeriod;
-//
-//    private int expiryTime;
-//
-//    private int passingDuration;
-//
-//    private static final long serialVersionUID = 1L;
-//
-//    public static final int SIDES = 6;
-//
-//    private GameCoordinate[] points;
-//
-//    private int radius;
-//
-//    private int rotation = 90;
-//
-//    private int xIndex;
-//
-//    private int yIndex;
-//
-//    private Polygon polygon;
-//
-//	public final static int HEXAGON_RADIUS = 30;
-//
-//    private Hexagon() {
-//    	  	points = new GameCoordinate[SIDES];
-//    	  	rotation = 90;
-//    	  	radius = HEXAGON_RADIUS;
-//    }
-//
-//
-//    private Hexagon(GameCoordinate center, int radius, int id, int xIndex, int yIndex, int guardPeriod, int expiryTime, int passingDuration) {
-//        this();
-//    		this.center = center;
-//        this.radius = radius;
-//        this.id = id;
-//        this.xIndex = xIndex;
-//        this.yIndex = yIndex;
-//        this.guardPeriod = guardPeriod;
-//        this.expiryTime = expiryTime;
-//        this.passingDuration = passingDuration;
-//        updatePoints();
-//    }
-//
-//    public Hexagon(double x, double y, int radius, int id, int guardPeriod, int expiryPeriod, int passingDuration) {
-//        this(new GameCoordinate(x, y), radius, id, 0, 0, guardPeriod, expiryPeriod, passingDuration);
-//    }
-//
-//    public Hexagon(double x, double y, int radius, int id, int xIndex, int yIndex, int guardPeriod, int expiryPeriod, int passingDuration) {
-//        this(new GameCoordinate(x, y), radius, id, xIndex, yIndex, guardPeriod, expiryPeriod, passingDuration);
-//    }
-//
-//    public int getRadius() {
-//        return radius;
-//    }
-//
-//    public void setRadius(int radius) {
-//        this.radius = radius;
-//        updatePoints();
-//    }
-//
-//    public int getRotation() {
-//        return rotation;
-//    }
-//
-//    public void setRotation(int rotation) {
-//        this.rotation = rotation;
-//
-//        updatePoints();
-//    }
-//
-//    public void setCenter(GameCoordinate center) {
-//        this.center = center;
-//
-//        updatePoints();
-//    }
-//
-//    public void setCenter(double x, double y) {
-//        setCenter(new GameCoordinate(x, y));
-//    }
-//
-//    public GameCoordinate getCenter() {
-//    		return center;
-//    }
-//
-//    private GameCoordinate findPoint(int p) {
-//    		double cosine, sine;
-//    		/**
-//	    		p: 0.0 xoff: -1.8369701987210297E-16 yoff: -1.0
-//			p: 1.0 xoff: 0.8660254037844384 yoff: -0.5000000000000004
-//			p: 2.0 xoff: 0.866025403784439 yoff: 0.4999999999999993
-//			p: 3.0 xoff: 3.061616997868383E-16 yoff: 1.0
-//			p: 4.0 xoff: -0.8660254037844388 yoff: 0.4999999999999998
-//			p: 5.0 xoff: -0.8660254037844392 yoff: -0.49999999999999917
-//    		 */
-//
-//    		switch (p) {
-//    		case 0:
-//    			sine = -1.0;
-//    			cosine = 0;
-//    			break;
-//    		case 1:
-//    			sine = -0.5;
-//    			cosine = 0.866025403784438d;
-//    			break;
-//    		case 2:
-//    			sine = 0.5d;
-//    			cosine = 0.866025403784438;
-//    			break;
-//    		case 3:
-//    			sine = 1.0;
-//    			cosine = 0;
-//    			break;
-//    		case 4:
-//    			sine = 0.5;
-//    			cosine = -0.866025403784438;
-//    			break;
-//    		case 5:
-//    			sine = -0.5;
-//    			cosine = -0.866025403784438;
-//    			break;
-//    		default:
-//    			throw new IllegalArgumentException();
-//    		}
-//    		double x = center.x + cosine * radius;
-//        double y = center.y + sine * radius;
-//
-//        return new GameCoordinate(x, y);
-//    }
-//
-//    protected void updatePoints() {
-//        for (int p = 0; p < SIDES; p++) {
-//            GameCoordinate point = findPoint(p);
-//            points[p] = point;
-//        }
-//
-//        polygon = new Polygon();
-//        polygon.add(new Point(points[0].x, points[0].y));
-//        polygon.add(new Point(points[1].x, points[1].y));
-//        polygon.add(new Point(points[2].x, points[2].y));
-//        polygon.add(new Point(points[3].x, points[3].y));
-//        polygon.add(new Point(points[4].x, points[4].y));
-//        polygon.add(new Point(points[5].x, points[5].y));
-//
-//    }
-//
-//    public double distanceTo(Hexagon otherHexagon) {
-//		return Math.sqrt(Math.pow(center.getX() - otherHexagon.getCenter().getX(), 2) + Math.pow(center.getY() - otherHexagon.getCenter().getY(), 2));
-//    }
-//
-//    public boolean isConnected(Hexagon otherHexagon) {
-//    		return this.distanceTo(otherHexagon) < (2 * HEXAGON_RADIUS);
-//    }
-//
-//	public int getId() {
-//		return id;
-//	}
-//
-//	public void setId(int id) {
-//		this.id = id;
-//	}
-//
-//	public String toString() {
-//		return ""+id;
-//	}
-//
-//	@Override
-//	public int hashCode() {
-//		return id;
-//	}
-//
-//	public int getExpiryTime() {
-//		return expiryTime;
-//	}
-//
-//	public void setExpiryTime(int expiryTime) {
-//		this.expiryTime = expiryTime;
-//	}
-//
-//	public int getGuardPeriod() {
-//		return guardPeriod;
-//	}
-//
-//	public GameCoordinate[] getPoints() {
-//		return points;
-//	}
-//
-//	public int getPassingDuration() {
-//		return passingDuration;
-//	}
-//
-//	public void setPassingDuration(int passingDuration) {
-//		this.passingDuration = passingDuration;
-//	}
-//
-//	public boolean contains(GameCoordinate coordinate) {
-//
-//		if(polygon == null) {
-//			updatePoints();
-//		}
-//
-//		return polygon.contains(new Point(coordinate.x, coordinate.y));
-//	}
-//}
+package com.codecontest.expo.teamexpo;
+
+public class Hexagon {
+
+	private int id;
+
+	private GameCoordinate center = new GameCoordinate(0, 0);
+
+	private int guardPeriod;
+
+    private int expiryTime;
+
+    private int passingDuration;
+
+    private static final long serialVersionUID = 1L;
+
+    public static final int SIDES = 6;
+
+    private GameCoordinate[] points;
+
+    private int radius;
+
+    private int rotation = 90;
+
+    private int xIndex;
+
+    private int yIndex;
+
+    private Polygon polygon;
+
+	public final static int HEXAGON_RADIUS = 30;
+
+    private Hexagon() {
+    	  	points = new GameCoordinate[SIDES];
+    	  	rotation = 90;
+    	  	radius = HEXAGON_RADIUS;
+    }
+
+
+    private Hexagon(GameCoordinate center, int radius, int id, int xIndex, int yIndex, int guardPeriod, int expiryTime, int passingDuration) {
+        this();
+    		this.center = center;
+        this.radius = radius;
+        this.id = id;
+        this.xIndex = xIndex;
+        this.yIndex = yIndex;
+        this.guardPeriod = guardPeriod;
+        this.expiryTime = expiryTime;
+        this.passingDuration = passingDuration;
+        updatePoints();
+    }
+
+    public Hexagon(double x, double y, int radius, int id, int guardPeriod, int expiryPeriod, int passingDuration) {
+        this(new GameCoordinate(x, y), radius, id, 0, 0, guardPeriod, expiryPeriod, passingDuration);
+    }
+
+    public Hexagon(double x, double y, int radius, int id, int xIndex, int yIndex, int guardPeriod, int expiryPeriod, int passingDuration) {
+        this(new GameCoordinate(x, y), radius, id, xIndex, yIndex, guardPeriod, expiryPeriod, passingDuration);
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+        updatePoints();
+    }
+
+    public int getRotation() {
+        return rotation;
+    }
+
+    public void setRotation(int rotation) {
+        this.rotation = rotation;
+
+        updatePoints();
+    }
+
+    public void setCenter(GameCoordinate center) {
+        this.center = center;
+
+        updatePoints();
+    }
+
+    public void setCenter(double x, double y) {
+        setCenter(new GameCoordinate(x, y));
+    }
+
+    public GameCoordinate getCenter() {
+    		return center;
+    }
+
+    private GameCoordinate findPoint(int p) {
+    		double cosine, sine;
+    		/**
+	    		p: 0.0 xoff: -1.8369701987210297E-16 yoff: -1.0
+			p: 1.0 xoff: 0.8660254037844384 yoff: -0.5000000000000004
+			p: 2.0 xoff: 0.866025403784439 yoff: 0.4999999999999993
+			p: 3.0 xoff: 3.061616997868383E-16 yoff: 1.0
+			p: 4.0 xoff: -0.8660254037844388 yoff: 0.4999999999999998
+			p: 5.0 xoff: -0.8660254037844392 yoff: -0.49999999999999917
+    		 */
+
+    		switch (p) {
+    		case 0:
+    			sine = -1.0;
+    			cosine = 0;
+    			break;
+    		case 1:
+    			sine = -0.5;
+    			cosine = 0.866025403784438d;
+    			break;
+    		case 2:
+    			sine = 0.5d;
+    			cosine = 0.866025403784438;
+    			break;
+    		case 3:
+    			sine = 1.0;
+    			cosine = 0;
+    			break;
+    		case 4:
+    			sine = 0.5;
+    			cosine = -0.866025403784438;
+    			break;
+    		case 5:
+    			sine = -0.5;
+    			cosine = -0.866025403784438;
+    			break;
+    		default:
+    			throw new IllegalArgumentException();
+    		}
+    		double x = center.x + cosine * radius;
+        double y = center.y + sine * radius;
+
+        return new GameCoordinate(x, y);
+    }
+
+    protected void updatePoints() {
+        for (int p = 0; p < SIDES; p++) {
+            GameCoordinate point = findPoint(p);
+            points[p] = point;
+        }
+
+        polygon = new Polygon();
+        polygon.add(new Point(points[0].x, points[0].y));
+        polygon.add(new Point(points[1].x, points[1].y));
+        polygon.add(new Point(points[2].x, points[2].y));
+        polygon.add(new Point(points[3].x, points[3].y));
+        polygon.add(new Point(points[4].x, points[4].y));
+        polygon.add(new Point(points[5].x, points[5].y));
+
+    }
+
+    public double distanceTo(Hexagon otherHexagon) {
+		return Math.sqrt(Math.pow(center.getX() - otherHexagon.getCenter().getX(), 2) + Math.pow(center.getY() - otherHexagon.getCenter().getY(), 2));
+    }
+
+    public boolean isConnected(Hexagon otherHexagon) {
+    		return this.distanceTo(otherHexagon) < (2 * HEXAGON_RADIUS);
+    }
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String toString() {
+		return ""+id;
+	}
+
+	@Override
+	public int hashCode() {
+		return id;
+	}
+
+	public int getExpiryTime() {
+		return expiryTime;
+	}
+
+	public void setExpiryTime(int expiryTime) {
+		this.expiryTime = expiryTime;
+	}
+
+	public int getGuardPeriod() {
+		return guardPeriod;
+	}
+
+	public GameCoordinate[] getPoints() {
+		return points;
+	}
+
+	public int getPassingDuration() {
+		return passingDuration;
+	}
+
+	public void setPassingDuration(int passingDuration) {
+		this.passingDuration = passingDuration;
+	}
+
+	public boolean contains(GameCoordinate coordinate) {
+
+		if(polygon == null) {
+			updatePoints();
+		}
+
+		return polygon.contains(new Point(coordinate.x, coordinate.y));
+	}
+}
--- app/src/main/java/com/codecontest/expo/teamexpo/MainActivity.java
+++ app/src/main/java/com/codecontest/expo/teamexpo/MainActivity.java
@@ -5,15 +5,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
 
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.ResponseBody;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,40 +39,52 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void fetchHex() {
-        Log.d("Response DATA:", "Mark");
-        new AsyncTask<Void, Void, String>() {
-            @Override
-            protected String doInBackground(Void... params) {
-                OkHttpClient client = new OkHttpClient();
-
-                Request request = new Request.Builder()
-                        .url("https://catcoder.codingcontest.org/api/contest/2060/rest/1066/movement/1")
-                        .get()
-                        .addHeader("authorization", "Basic dGVhbV9FeHBvOlRlYW0xMjM0NQ==")
-                        .build();
-
-
-
-                try {
-                    Response response = client.newCall(request).execute();
-                    String responseString = response.body().string();
-                    Log.d("Response DATA:", responseString);
+//        Log.d("Response DATA:", "Mark");
+//        new AsyncTask<Void, Void, String>() {
+//            @Override
+//            protected String doInBackground(Void... params) {
+//                OkHttpClient client = new OkHttpClient();
+//
+//                Request request = new Request.Builder()
+//                        .url("https://catcoder.codingcontest.org/api/contest/2060/rest/1067/playground/1")
+//                        .get()
+//                        .addHeader("authorization", "Basic dGVhbV9FeHBvOlRlYW0xMjM0NQ==")
+//                        .build();
+//
+//
+//
+//                try {
+//                    Response response = client.newCall(request).execute();
+//                    InputStream instream = response.body().byteStream();
+//
+//                    StringBuilder buffer = new StringBuilder();
+//
+//                    BufferedReader reader = new BufferedReader(new InputStreamReader(instream));
+//
+//                    String line = null;
 //                    try {
-//                        JSONObject jsonData = new JSONObject(responseString);
-//                        JSONObject testData = new JSONObject(jsonData.getString("outputs"));
-//                        String testData2 = testData.getString("poa");
-//                        String acDataString = testData2.substring(1);
-//                        Log.d("testData", testData2);
+//                        while ((line = reader.readLine()) != null) {
+//                            buffer.append(line);
+//                        }
 //
-//                        final String []acData = acDataString.split(",");
-//                        // Log.d("DataInStringArray", acData[13]);
-//                        runOnUiThread(new Runnable() {
-//                            @Override
-//                            public void run() {
-//                            Log.d( "run()", Arrays.toString(acData) );
+//                    } finally {
+//                        instream.close();
+//                        reader.close();
+//                    }
 //
-//                            }
-//                        });
+//                    try {
+//                        JSONObject jsonData = new JSONObject(buffer.toString());
+//                        JSONObject testData = new JSONObject(jsonData.getString("userTraces"));
+//                        Log.d("Mark", testData.toString());
+//////                        final String []acData = acDataString.split(",");
+////                        // Log.d("DataInStringArray", acData[13]);
+////                        runOnUiThread(new Runnable() {
+////                            @Override
+////                            public void run() {
+//////                            Log.d( "run()", Arrays.toString(acData) );
+////
+////                            }
+////                        });
 //
 //
 //                        //TODO we should make an array that accepts the data coming from the JSON response
@@ -66,12 +92,79 @@ public class MainActivity extends AppCompatActivity {
 //                        e.printStackTrace();
 //                    }
 //                    return response.body().string();
-                } catch (IOException e) {
-                    e.printStackTrace();
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//                return null;
+//            }
+//        }.execute();
+
+
+        String json = null;
+        try {
+            InputStream is = getAssets().open("movement.json");
+            int size = is.available();
+            byte[] buffer = new byte[size];
+            is.read(buffer);
+            is.close();
+            json = new String(buffer, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return;
+        }
+        String json2 = null;
+        try {
+            InputStream is2 = getAssets().open("playground.json");
+            int size = is2.available();
+            byte[] buffer2 = new byte[size];
+            is2.read(buffer2);
+            is2.close();
+            json2 = new String(buffer2, "UTF-8");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            return;
+        }
+
+        Gson gson = new Gson();
+        try {
+            JsonElement obj = gson.fromJson(json, JsonElement.class);
+            JsonArray obj2 = obj.getAsJsonArray();
+
+            JsonElement playobj = gson.fromJson(json2, JsonElement.class);
+            JsonArray playobj2 = playobj.getAsJsonArray();
+            String res = "";
+            for(int i=0; i<obj2.get(0).getAsJsonObject().get("userTraces").getAsJsonArray().size();i++){
+
+            JsonArray arr = obj2.get(0).getAsJsonObject().get("userTraces").getAsJsonArray().get(i).getAsJsonArray();
+                for(int j=0; j<arr.size();j++){
+                    JsonObject arr2 = arr.get(j).getAsJsonObject();
+                    double x = arr2.get("x").getAsDouble();
+                    double y = arr2.get("y").getAsDouble();
+                    Point p = new Point(x,y);
+//                    Log.d("Mark", x + " " + y);
+                    for(int k = 0; k<playobj2.size();k++){
+                        Hexagon hex = new Hexagon(playobj2.get(k).getAsJsonObject().get("center").
+                                getAsJsonObject().get("x").getAsDouble(),playobj2.get(k).getAsJsonObject().get("center").
+                                getAsJsonObject().get("y").getAsDouble(),
+                               30, playobj2.get(k).getAsJsonObject().get("id").getAsInt(),1,1,1);
+                        boolean contain = hex.contains(new GameCoordinate(x,y));
+                        if(contain && !res.contains(hex.getId()+"")){
+                            res += hex.getId() + " ";
+
+                        }
+                    }
+
                 }
-                return null;
+
+
             }
-        }.execute();
+
+            Log.d("Mark", res);
+
+            //Log.d("Mark", obj2.get(0).getAsJsonObject().get("userTraces").getAsJsonArray().get(0) + "");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
     }
 }
