6cc23ad72023e76dd44c756ddfb4be51280f0db4
markragaee <markragaee@gmail.com>
2017-10-20 15:13:46 +0200

    1st commit

--- app/src/main/java/com/codecontest/expo/teamexpo/MainActivity.java
+++ app/src/main/java/com/codecontest/expo/teamexpo/MainActivity.java
@@ -25,43 +25,47 @@ public class MainActivity extends AppCompatActivity {
     }
 
     void fetchHex() {
-        final String BASE_URL = "https://catcoder.codingcontest.org/api/contest/2060/rest/1067/playground/1";
+        Log.d("Response DATA:", "Mark");
         new AsyncTask<Void, Void, String>() {
             @Override
             protected String doInBackground(Void... params) {
                 OkHttpClient client = new OkHttpClient();
+
                 Request request = new Request.Builder()
-                        .url(BASE_URL)
-                        .addHeader("Authorization", "Basic dGVhbV9FeHBvOlRlYW0xMjM0NQ==")
+                        .url("https://catcoder.codingcontest.org/api/contest/2060/rest/1066/movement/1")
+                        .get()
+                        .addHeader("authorization", "Basic dGVhbV9FeHBvOlRlYW0xMjM0NQ==")
                         .build();
+
+
 
                 try {
                     Response response = client.newCall(request).execute();
                     String responseString = response.body().string();
-                    Log.d("Response DATA:", response.body().string());
-                    try {
-                        JSONObject jsonData = new JSONObject(responseString);
-                        JSONObject testData = new JSONObject(jsonData.getString("outputs"));
-                        String testData2 = testData.getString("poa");
-                        String acDataString = testData2.substring(1);
-                        Log.d("testData", testData2);
-
-                        final String []acData = acDataString.split(",");
-                        // Log.d("DataInStringArray", acData[13]);
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                            Log.d( "run()", Arrays.toString(acData) );
-
-                            }
-                        });
-
-
-                        //TODO we should make an array that accepts the data coming from the JSON response
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-                    return response.body().string();
+                    Log.d("Response DATA:", responseString);
+//                    try {
+//                        JSONObject jsonData = new JSONObject(responseString);
+//                        JSONObject testData = new JSONObject(jsonData.getString("outputs"));
+//                        String testData2 = testData.getString("poa");
+//                        String acDataString = testData2.substring(1);
+//                        Log.d("testData", testData2);
+//
+//                        final String []acData = acDataString.split(",");
+//                        // Log.d("DataInStringArray", acData[13]);
+//                        runOnUiThread(new Runnable() {
+//                            @Override
+//                            public void run() {
+//                            Log.d( "run()", Arrays.toString(acData) );
+//
+//                            }
+//                        });
+//
+//
+//                        //TODO we should make an array that accepts the data coming from the JSON response
+//                    } catch (JSONException e) {
+//                        e.printStackTrace();
+//                    }
+//                    return response.body().string();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
